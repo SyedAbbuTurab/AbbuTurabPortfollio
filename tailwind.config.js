18d070333f860ba0c865ea98bf2764ac7355e145
Abbu Turab <syedabbuturab786@gmail.com>
2025-06-10 20:58:51 -0500

    Added resume and removed the unwanted optional codes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx}"],
+  content: ["./src/**/*.{html,js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        signature: ['Great Vibes']
+      }
+    },
   },
   plugins: [],
-};
+}
